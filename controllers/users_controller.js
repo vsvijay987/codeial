2c8b175dd58909394053a6a820ceb20cb70da24f
vsvijay987 <vsvijju987@gmail.com>
2020-02-06 13:26:56 +0530

    setup static files to access layouts and pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile = function(req, res){
-    res.end("<h1>user profile</h1>")
+    return res.render('users_profile', {
+        title: 'profile'
+    })
 }
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayout = require('express-ejs-layouts');
 
+app.use(express.static('./assets'))
+
+app.use(expressLayout);
+
+//extract style and script from subpages into layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+//use express router
 app.use('/', require('./routes'));
 
+//setup the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
