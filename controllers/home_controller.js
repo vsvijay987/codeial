e57f465b8b5bc7aa934048d0d44ec047ed0de1c9
vsvijay987 <vsvijju987@gmail.com>
2020-02-15 10:13:49 +0530

    Saving Posts to database

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,15 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    console.log(req.cookies)
+    //console.log(req.cookies)
     //res.cookie('user_id', 25)
-    return res.render('home', {
-        title: "Home"
-    })
+    Post.find({}, function(err, posts){
+        return res.render('home', {
+            title: "Home | Codeial",
+            posts: posts
+        });
+    });
+    
 }
 
 // module.exports.actionName = function(req, res){}
