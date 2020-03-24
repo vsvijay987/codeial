7b0a5d5564c9bd8b7be968101769192e7bcb4769
vsvijay987 <vsvijju987@gmail.com>
2020-03-24 17:59:00 +0530

    adding likes to comments and post

--- models/post.js
+++ models/post.js
@@ -17,6 +17,13 @@ const postSchema = new mongoose.Schema({
             type:  mongoose.Schema.Types.ObjectId,
             ref: 'Comment'
         }
+    ],
+
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Like'
+        }
     ]
 },{
     timestamps: true
